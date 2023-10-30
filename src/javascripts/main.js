7af20550ce981ffa3cc41f8a1e2dff707d29d062
Coleton Watt <electricninja102@gmail.com>
2023-10-30 13:20:22 -0600

    Made a from and learned bootstrap

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,2 +1,11 @@
 //TODO - Your ES6 JavaScript code (if any) goes here
 import "bootstrap"
+
+import { addGuest } from "./guest"
+
+
+document.querySelector("#guest-form").onsubmit = addGuest;
+
+d
+
+
